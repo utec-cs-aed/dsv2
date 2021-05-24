7e723b412f621b198b7e819bf5d1f768fd0335dc
Heider Sanchez <heider.sanchez.pe@gmail.com>
2021-05-24 17:47:21 -0500

    fix main

--- main.cpp
+++ main.cpp
@@ -10,13 +10,13 @@ using namespace std;
 
 void testDSTree(vector<char> data){    
     DisjoinSet<char>* ds = new DisjoinSetTree<char>(data);
-    ASSERT(ds->sets() == 8, "The function sets is not working");
+    ASSERT(ds->sets() == 8, "The function MakeSet is not working");
     ds->Union(0, 2);
     ds->Union(5, 6);
     ds->Union(1, 6);
     ds->Union(7, 0);
     ds->Union(4, 3);
-    ASSERT(ds->sets() == 3, "The function sets is not working");
+    ASSERT(ds->sets() == 3, "The function Union is not working");
     ASSERT(ds->isConnected(1, 5) == true, "The function isConnected is not working");
     ASSERT(ds->isConnected(2, 4) == false, "The function isConnected is not working");
     ASSERT(ds->isConnected(0, 6) == false, "The function isConnected is not working");
@@ -25,6 +25,10 @@ void testDSTree(vector<char> data){
     sort(set1.begin(), set1.end());
     ASSERT(set1[0] == 'e', "The function getElementsSet is not working");
     ASSERT(set1[1] == 'd', "The function getElementsSet is not working");
+
+    ds->Union(2, 5);
+    ds->Union(3, 1);
+    ASSERT(ds->sets() == 1, "The function sets is not working");
     
     delete ds;
 }
@@ -46,6 +50,10 @@ void testDSArray(vector<char> data){
     sort(set1.begin(), set1.end());
     ASSERT(set1[0] == 'e', "The function getElementsSet is not working");
     ASSERT(set1[1] == 'd', "The function getElementsSet is not working");
+
+    ds->Union(2, 5);
+    ds->Union(3, 1);
+    ASSERT(ds->sets() == 1, "The function sets is not working");
     
     delete ds;
 }
